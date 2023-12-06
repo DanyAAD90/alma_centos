c93661585ec65a43d2eeb5d903e87fbcfc18714f
DanyAAD90 <117837948+DanyAAD90@users.noreply.github.com>
2023-12-06 09:00:00 +0100

    Update 5qgis.sh

--- 5qgis.sh
+++ 5qgis.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-su
-bash Miniconda3-latest-Linux-x86_64.sh -bfp /miniconda
+bash Miniconda3-latest-Linux-x86_64.sh -bfp /miniconda -y
 /miniconda/condabin/conda update -n base -c defaults conda
 /miniconda/condabin/conda init bash
 conda config --set auto_activate_base false
@@ -11,3 +10,4 @@ conda create --name qgis_env
 conda activate qgis_env
 conda install qgis matplotlib numpy -c conda-forge
 qgis
+#----- jeszcze przetestowac kilka razy------
